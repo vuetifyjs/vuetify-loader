f6b618cb1e24486571571355b6d4d5be73175bec
nekosaur <albert@kaaman.nu>
2017-09-16 21:06:35 +0200

    updated loader

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const loaderUtils = require("loader-utils")
 
 module.exports = function(source) {
   const options = loaderUtils.getOptions(this)
-
   if (options.theme && this.resourcePath.endsWith('.styl')) {
-    if (this.resourcePath.includes('node_modules/vuetify') || options.globalImport) {
+    if (this.resourcePath.includes('vuetify/src') || options.globalImport) {
       source = `@import '${options.theme}'\n${source}`
     }
   }
