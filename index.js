ccc7ea8f305fb06a1a37c88c7997e25d812e9895
nekosaur <albert@kaaman.nu>
2017-10-25 18:52:48 +0200

    fix: os agnostic path separator
    path to vuetify src folder was previously hardcoded with unix path separator
    
    fixes #1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const loaderUtils = require("loader-utils")
+const vuetifySrc = require('path').join('vuetify', 'src')
 
 module.exports = function(source) {
   const options = loaderUtils.getOptions(this)
   if (options.theme && this.resourcePath.endsWith('.styl')) {
-    if (this.resourcePath.includes('vuetify/src') || options.globalImport) {
+    if (this.resourcePath.includes(vuetifySrc) || options.globalImport) {
       source = `@import '${options.theme}'\n${source}`
     }
   }
